e0c61093d9c9a353889a5be1a59a5e68dc332d48
Romango <homer37@yandex.ru>
2022-05-29 06:58:06 +0400

    aaaa

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import SmallSlider from "../components/smallSlider/SmallSlider";
+import SmallSlider from "../components/SmallSlider/SmallSlider";
 import { useState } from "react";
 
 
